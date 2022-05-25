6fbba46fc3dd24e93049cf5f36cf187258c11665
Promethea Raschke <promethea.raschke@keep.network>
2022-05-25 15:36:08 +0100

    Fix length references in extractHashAt comments

--- solidity/contracts/BTCUtils.sol
+++ solidity/contracts/BTCUtils.sol
@@ -583,8 +583,8 @@ library BTCUtils {
         uint8 _scriptLen = uint8(_output[_at]);
 
         // don't have to worry about overflow here.
-        // if _scriptLen + 9 overflows, then output length would have to be < 9
-        // for this check to pass. if it's < 9, then we errored when assigning
+        // if _scriptLen + 1 overflows, then output length would have to be < 1
+        // for this check to pass. if it's < 1, then we errored when assigning
         // _scriptLen
         if (_scriptLen + 1 != _len) {
             return hex"";
